12306424e196fcd1ca3f942546a53587eb15192f
NowScott <nowscott@qq.com>
2024-04-12 12:36:18 +0800

    vault backup: 2024-04-12 12:36:18

--- toc.py
+++ toc.py
@@ -11,27 +11,23 @@ def generate_directory_tree(directory, depth=0, ignored_folders=set(), ignored_f
     files = os.listdir(directory)
     directories = [f for f in files if os.path.isdir(os.path.join(directory, f))]
     files = [f for f in files if os.path.isfile(os.path.join(directory, f)) and f not in ignored_files]
-    
     directories.sort(reverse=True)  # 倒序排序文件夹
     files.sort(key=natural_sort_key, reverse=True)  # 倒序排序文件
-    
     for d in directories:
         if d not in ignored_folders:
             tree += "  " * depth + "- {}\n".format(d)
-            tree += generate_directory_tree(os.path.join(directory, d), depth + 1, ignored_folders, ignored_files, base_path)
-                
+            tree += generate_directory_tree(os.path.join(directory, d), depth + 1, ignored_folders, ignored_files, base_path)      
     for f in files:
         relative_path = os.path.relpath(os.path.join(directory, f), base_path)
         relative_path = urllib.parse.quote(relative_path)  # 将路径中的空格转换为%20
         tree += "  " * depth + "- [{}]({})\n".format(f, relative_path)
-    
     return tree
 
 # 获取当前文件夹下的所有文件
 current_directory = os.getcwd()
 
 # 指定要忽略的文件夹和文件
-ignored_folders = {'.github', 'Temp', '.git','.obsidian'}  
+ignored_folders = {'.github', 'Temp', '.git','.obsidian','asset'}  
 ignored_files = {'toc.py', '.DS_Store', 'README.md', '.gitignore'}
 
 # 读取README.md文件内容
